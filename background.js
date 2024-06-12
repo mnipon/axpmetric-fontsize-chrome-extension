3ca4c1493340c269e6a6a3cf1d237cd3022761b7
mnipon <78396926+mnipon@users.noreply.github.com>
2024-06-12 11:13:02 +0700

    Shared commit

--- background.js
+++
@@ -1,9 +0,0 @@
-// background.js
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["content.js"],
-    });
-  });
-});
